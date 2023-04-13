7157320e514961b3885467cae3e45101e6580532
BrianLupu93 <brianlupu@gmail.com>
2023-04-13 21:56:10 +0200

    test mongoose schema and model

--- server.js
+++ server.js
@@ -12,6 +12,43 @@ mongoose.connect(DB).then((con) => {
   console.log('DB connection established!');
 });
 
+// create a schema
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour needs a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour needs a price'],
+  },
+});
+
+// create a model
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The test tour',
+  rating: 4.7,
+  price: 489,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
