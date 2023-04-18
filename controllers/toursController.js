c965898a7bd04bfad2eab17660221f6b7d0a9cb4
BrianLupu93 <brianlupu@gmail.com>
2023-04-18 21:38:02 +0200

    Build and execute query

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -60,7 +60,18 @@ exports.getTour = async (req, res) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Buidl the query
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+    // const query = Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+
+    // Execute the query
+    const tours = await query;
+
+    // Send Rescponse
     res.status(200).json({
       status: 'success',
       results: tours.length,
